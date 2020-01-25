83844c2763ac4574d6158f5353a764f87d7fc408
jjoshc714 <jjoshc714@yahoo.com>
2020-01-25 13:16:12 -0800

    Added notes to lines of function

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,30 @@
 ## The functions cache the inverse of a matrix, which may be more useful than computing 
 ## multiple matrices over and over. 
 
-## This function stores four functions: 1. set matrix as special object in a separate
-## environment, 2. get "1.", 3. set "i" as matrix inverse, and 4. get "i" in a list.
+## This function stores a matrix and computed inverse into a separate environment.
 
 makeCacheMatrix <- function(x = matrix()){
-      i <- NULL
-      set <- function(y) {
-            x <<- y
-            i <<- NULL
-      }
-      get <- function() x
-      setinverse <- function(solve) i <<- solve
-      getinverse <- function() i
-      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-}
+   i <- NULL                     #creates empty object "i" as placeholder for inverse
+   set <- function(y) {          #set matrix as special object in separate environment
+      x <<- y
+      i <<- NULL
+   }
+   get <- function() x           #gets result of set
+   setinverse <- function(solve) i <<- solve #set "i" as matrix inverse 
+   getinverse <- function() i    #gets result of setinverse
+   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}                                #creates list containing previous four functions
 
-## This function returns solved i from previous function unless empty: then gets back
-## the stored matrix, solves its inverse as "i", and stores it back.
+## This function returns solved "i" from previous function unless if empty; recomputes
 
 cacheSolve <- function(x, ...) {
-      i <- x$getinverse()
-      if(!is.null(i)) {
-            message("getting cached data")
-            return(i)
-      }
-      data <- x$get()
-      i <- solve(data, ...)
-      x$setinverse(i)
-      i
+   i <- x$getinverse()           #stores "i" as computed inverse from previous function
+   if(!is.null(i)) {             #if "i" not empty, return message and value
+      message("getting cached data")
+      return(i)
+   }
+   data <- x$get()               #store matrix from separate environment into "data"
+   i <- solve(data, ...)         #store inverse of "data" into "i" 
+   x$setinverse(i)               #store "i" back into separate environment as "setinverse"
+   i
 }
